afba789b8167b945dc06b1d795861aab302836a6
Alessandro Razeto <razeto@lngs.infn.it>
2018-11-12 09:06:03 +0100

    plot

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3.6
 import configparser as cp
 import numpy as np
+import matplotlib.pyplot as plt
 from sipm import SiPM
 
 c = cp.ConfigParser()
@@ -16,10 +17,15 @@ s = SiPM(c['__current__'])
 
 #s.add_pes(np.random.uniform(0, 17e-6, 10)) 
 s.add_pes(0)
-#s.add_dcr()
+s.add_dcr()
 s.trigger()
-#s.add_noises()
+s.add_noises()
 
 print (*s.pe_list, sep="\n")
 
-s.wav()
+w = s.wav()
+plt.plot(w)
+plt.ylabel('waveform [bins]')
+plt.xlabel('time [samples]')
+plt.show()
+
