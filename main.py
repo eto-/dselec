df6ad0988d2820acdb7c1a2b1ef59aff6c2f43f9
Alessandro Razeto <razeto@lngs.infn.it>
2018-12-23 16:46:17 +0100

    Code reorganized: phct in sipm + specialized file name + better info on spe

--- main.py
+++ main.py
@@ -25,36 +25,20 @@ class Main:
         self.s = SiPM(self.c['__current__'])
 
 
-        self.phct = float(self.c['__current__']['phct'])
-
-
-    def __phct(self):
-        r = 1
-        for i in range(0, np.random.poisson(self.phct)): r += self.__phct()
-        return r
-
-    def _phct(self, n):
-        if self.phct <= 0: return n
-        r = 0
-        for i in range(0, n): r += self.__phct()
-        return r
-
     def test_loop(self, n, npe=1, noises=True, rate=100):
         bs = float(self.c['__current__']['baseline'])
         v_sum = []
         v_max = []
         v_sd = []
-        for i in range(0, n):
+        for i in range(n):
             self.s.pe_list.clear()
 
             if npe < 0: self.s.add_pes(np.zeros(-npe))
-            elif npe > 0: self.s.add_pes(np.random.exponential(1.5e-6, self._phct(np.random.poisson(npe))))
- 
-            if noises: self.s.add_dcr()
+            elif npe > 0: self.s.add_pes(np.random.exponential(1.5e-6, np.random.poisson(npe)))
+            
+            if noises: self.s.add_noises()
 
             self.s.trigger(not noises)
-
-            if noises: self.s.add_noises()
 
             w = self.s.wav()
 
@@ -64,26 +48,29 @@ class Main:
 
             self.o.write(np.random.exponential(1/rate), w)
 
-        with open('peak.txt', 'w') as f: 
+        with open('peak_%.1f_%s.txt' % (npe, noises), 'w') as f: 
             for i in v_sum: 
                 f.write("%d\n" % i)
-        return (np.mean(v_sum), np.std(v_sum), np.mean(v_max), np.mean(v_sd))
+        return [np.mean(v_sum), np.std(v_sum), np.mean(v_max), np.mean(v_sd)]
 
 
 
 m = Main(None, None)
 
-it = 50000
-spe = m.test_loop(it, -1, False)
+it = 5000
+spe = m.test_loop(it, -1, True)
+spe2 = m.test_loop(it, -1, False)
+spe[0] = spe2[0]
 print("SPE mean charge = %.2f with sd = %.2f (spread = %.2f)" % (spe[0], spe[1], spe[1]/spe[0]))
 
-n = 220
+it = 50000
+n = 1.6
 sim = m.test_loop(it, n, True)
-print("Simulated %d npe (phct = %d)" % (n, n * 1/(1 - m.phct)))
+print("Simulated %.1f npe (phct = %.1f)" % (n, n * 1/(1 - m.s.phct)))
 print("Total npe = %.2f with sd = %.2f (spread = %.2f <-> expected %.2f)" % (sim[0] / spe[0], sim[1] / spe[0], sim[1]/sim[0], np.sqrt(spe[0] / sim[0])))
 
 
-#cProfile.run('m.test_loop(5000, 100, True)')
+#cProfile.run("m.test_loop(1000, n, True)")
 #print("mean2=" + str(np.mean(v2)))
 
 #plt.plot(w)
