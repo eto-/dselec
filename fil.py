a0dfadee87356ee43dc6861722cff15fa9adbb5d
Alessandro Razeto <razeto@lngs.infn.it>
2018-12-26 16:45:35 +0100

    handle no input

--- fil.py
+++ fil.py
@@ -26,13 +26,16 @@ class HEADER:
 
 class FIL:
     def __init__(self, conf):
-        self.file_name = conf['g4ds']
+        self.file_name = conf['input']
+        self.dummy = self.file_name == 'none'
+        if (self.dummy) return
+
         self.file = open(self.file_name, "rb")
         self.header = HEADER(self.__get(HEADER_FMT))
         self.n = 0
 
     def __del__(self):
-        self.file.close()
+        if not self.dummy: self.file.close()
 
     def __io(self, size):
         b = self.file.read(size)
@@ -58,10 +61,10 @@ class FIL:
             self.file.seek(calcsize(fmt) * n, 1)
 
     def read(self):
+        if self.dummy: return []
+
         size = self.__get('i', False)[0]
         event = self.__get(EVENT_FMT, False)
-#        print("event:")
-#        print(event[20:27])
 
         self.__skip(DAUGHTER_FMT, event[24])
         self.__skip(DEPOSIT_FMT, event[25])
@@ -80,6 +83,8 @@ class FIL:
         return pes
 
     def __next__(self):
+        if self.dummy: return self.read()
+
         if (self.header.events > 0 and self.n >= self.header.events):
             raise StopIteration
         self.n = self.n + 1
