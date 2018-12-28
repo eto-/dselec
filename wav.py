8a25a0e03c4da50d3ba6b07fe622dc8395ee359e
Alessandro Razeto <razeto@lngs.infn.it>
2018-12-28 13:11:21 +0100

    New V2.00 format

--- wav.py
+++ wav.py
@@ -13,25 +13,36 @@ class WAV:
         self.file.setframerate(int(float(conf['sampling'])/1e6))
         self.file.setcomptype('NONE', 'not compressed')
         self.counter = 0
-        self.cumulative_t = 0
+        self.time = 0
+        self.rate = float(conf.get('rate', '100'))
 
     def __del__(self):
         if not self.dummy:
             self.file.close()
 
-    def write(self, t, w):
+    def write(self, e, ws):
         if self.dummy: return
 
-        self.cumulative_t += t
-        self.counter += 1
+        if e is None:
+            self.time += np.random.exponential(1 / self.rate)
+            self.counter += 1
+        else:
+            self.time = e.time
+            self.counter = e.id
 
-        t = int(self.cumulative_t / 8e-9) % 0xFFFFFFFF
+        data_len = sum(map(lambda w: 4 + w.wav.size, ws))
+
+        time_tag = int(self.time / 8e-9) % 0xFFFFFFFF
+        cpu_time_ms = int(round(self.time * 1e3))
         #uint16_t marker, header_length;
-        f = b''.join([wav.struct.pack('H', i) for i in [0xffff, 20]])
-        #uint32_t counter, time_tag, n_samples, cpu_time_ms, n_channels, unused[4];
-        f = f + b''.join([wav.struct.pack('I', i) for i in [self.counter, t, w.size, int(round(self.cumulative_t * 1e3)), 1, 0, 0, 0, 0]])
-        #uint16_t samples[0];
-        f = f + b''.join([wav.struct.pack('h', i) for i in w])
+        #uint32_t counter, time_tag, n_samples, cpu_time_ms;
+        #uint16_t n_channels, version;
+        #uint32_t data_length, unused[3]
+        f = wav.struct.pack('=2H4I2H4I', *(0xffff, 20, self.counter, time_tag, 0, cpu_time_ms, len(ws), 200, data_len, 0, 0, 0))
+        for w in ws:
+            #uint16_t channel, n_samples, unused[2];
+            f = f + wav.struct.pack('=4H', *(w.id, w.wav.size, 0, 0))
+            f = f + b''.join([wav.struct.pack('h', i) for i in w.wav])
 
         self.file.writeframes(f)
 
